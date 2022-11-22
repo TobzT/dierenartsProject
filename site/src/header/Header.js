97c519e90640e3723517e1c52c267d8fdb9d0a1a
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-22 12:17:34 +0100

    header

--- site/src/header/Header.js
+++ site/src/header/Header.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Icon } from "@iconify/react";
+import "./Header.css";
 
 export default function header() {
-  return <div>This is the header</div>;
+  return (
+    <div className="Header">
+      <div className="logo">
+        <Icon icon="noto:paw-prints" className="logo-img" />
+        <span className="logo-name">Woof</span>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Plan afspraak</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
 }
