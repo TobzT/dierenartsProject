f00285f3925b1d57bdd1548c7d615a75e7dcc606
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-22 10:54:31 +0100

    create folder system react

---
+++ site/src/AppointmentAvailable/AppointmentAvailable.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function appointmentAvailable() {
+  return <div>This is the appointment availability</div>;
+}
---
+++ site/src/appointmentForm/AppointmentForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function appointmentForm() {
+  return <div>This is the appointmentForm</div>;
+}
